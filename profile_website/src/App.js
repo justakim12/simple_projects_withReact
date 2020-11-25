c950ac99e443dda3ceda031f396dae4105c55403
JustaKim <rlaruddlf711@naver.com>
2020-11-26 00:10:29 +0900

    Add header and basic framework

--- profile_website/src/App.js
+++ profile_website/src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Header.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body> 
+      <Header></Header>
+      <br></br>
+      <div class="content">
+        Make page scrollable.
+      </div>
+      <div class="content">
+        Make page scrollable.
+      </div>
+      <div class="content">
+        Make page scrollable.
+      </div>
+      <div class="content">
+        Make page scrollable.
+      </div>
+      <div class="content">
+        Make page scrollable.
+      </div>
+      <div class="content">
+        Make page scrollable.
+      </div>
+      <div class="content">
+        Make page scrollable.
+      </div>
+      <div class="content">
+        Make page scrollable.
+      </div>
+      <div class="content">
+        Make page scrollable.
+      </div>
+      <div class="content">
+        Make page scrollable.
+      </div>
+      <div class="content">
+        Make page scrollable.
+      </div>
+      <div class="content">
+        Make page scrollable.
+      </div>
+      <div class="content">
+        Make page scrollable.
+      </div>
+      <div class="content">
+        Make page scrollable.
+      </div>
+      <div class="content">
+        Make page scrollable.
+      </div>
+    </body>
   );
 }
 
