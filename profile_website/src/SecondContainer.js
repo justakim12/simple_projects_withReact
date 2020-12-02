c756d58f7321e0ab51ecdced516053fe97ee0f1f
JustaKim <rlaruddlf711@naver.com>
2020-12-02 09:57:00 +0900

    Add first card-box

--- profile_website/src/SecondContainer.js
+++ profile_website/src/SecondContainer.js
@@ -1,6 +1,15 @@
 const SecondContainer = () => {
     return (
         <div className="container-box">
+            <div className="firstContainer-textbox">
+                PROJECTS
+            </div>
+            <div className="card-box">
+                
+
+
+
+            </div>
         </div>   
     )
 }
