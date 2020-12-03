fa8ba0c88758eb61674be57b9c0cfa0fdd43775b
JustaKim <rlaruddlf711@naver.com>
2020-12-03 22:23:32 +0900

    Add project cards

--- profile_website/src/SecondContainer.js
+++ profile_website/src/SecondContainer.js
@@ -4,11 +4,15 @@ const SecondContainer = () => {
             <div className="firstContainer-textbox">
                 PROJECTS
             </div>
-            <div className="card-box">
-                
-
-
-
+            <div className="card-boxContainer">
+                <div className="card-box1">
+                </div>
+                <div className="card-box2">
+                </div>
+                <div className="card-box3">
+                </div>
+                <div className="card-box4">
+                </div>
             </div>
         </div>   
     )
