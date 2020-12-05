fb7f33ef6ac4a461f5e890035410638f32a63280
JustaKim <rlaruddlf711@naver.com>
2020-12-05 14:16:44 +0900

    Add skillset image logos

--- profile_website/src/ThirdContainer.js
+++ profile_website/src/ThirdContainer.js
@@ -1,6 +1,9 @@
 import python_logo from './Images/python.png'
 import sql_logo from './Images/sql.png'
 import tableau_logo from './Images/tableau.png'
+import hive_logo from './Images/hive.png'
+import spark_logo from './Images/spark.png'
+import react_logo from './Images/react.png'
 
 const ThirdContainer = () => {
     return (
@@ -10,9 +13,14 @@ const ThirdContainer = () => {
             </div>
             <div className="skillset-boxContainer">
                 <div className="skillsetImage-box">
-                    <img className="firstLogo-image" src={python_logo}></img>
-                    <img className="secondLogo-image" src={sql_logo}></img>
-                    <img className="thirdLogo-image" src={tableau_logo}></img>
+                    <img className="firstLogo-image" src={python_logo} alt="logoimage"></img>
+                    <img className="secondLogo-image" src={sql_logo} alt="logoimage"></img>
+                    <img className="thirdLogo-image" src={tableau_logo} alt="logoimage"></img>
+                </div>
+                <div className="skillsetImage-box">
+                    <img className="firstLogo-image" src={hive_logo} alt="logoimage"></img>
+                    <img className="secondLogo-image" src={spark_logo} alt="logoimage"></img>
+                    <img className="thirdLogo-image" src={react_logo} alt="logoimage"></img>
                 </div>
             </div>
         </div>   
