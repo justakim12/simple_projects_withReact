dd9416ab7336ba422967190cad32e472ae3084b6
JustaKim <rlaruddlf711@naver.com>
2020-12-09 09:20:19 +0900

    Fixing timeline

--- profile_website/src/FourthContainer.js
+++ profile_website/src/FourthContainer.js
@@ -6,16 +6,10 @@ const FourthContainer = () => {
             </div>
             <div className="timeline-boxContainer">
                 <div className="timeline">
-                    <div className="container-left1">
-                        <div className="container-left1-text">
-                            Present
-                        </div>
-                    </div>
-                    <div className="container-right1">
-                        <div className="container-right1-text">
-                            <li>
-                                Product Manager and Data Analyst at Naver Corp, GLACE department    
-                            </li>
+                    <div className="container left">
+                        <div className="content">
+                            <h2>Present</h2>
+                            <p>Project Manager at Naver</p>
                         </div>
                     </div>
                 </div>
