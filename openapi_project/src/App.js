3ad4f1426b97c5c5fe5333e5f3e605f57cf97da9
JustaKim <rlaruddlf711@naver.com>
2020-12-13 12:50:13 +0900

    Add base HTML

--- openapi_project/src/App.js
+++ openapi_project/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="date-box">
+      <div className="date">
+        20201209
+      </div>
+      <div className="date">
+        20201210
+      </div>
+      <div className="date">
+        20201211
+      </div>
+      <div className="date">
+        20201212
+      </div>
+      <div className="result">
+        누적 확잔자 숫자: 000명
+      </div>
     </div>
   );
 }
